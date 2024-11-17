e29904ac968096e5f23b6fc50548e592cfa9096c
Mikhail <pochta.mihaila@gmail.com>
2024-11-17 13:52:17 +0300

    17.11.2024

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+homework_complet=12
+hours_spent=1.5
+name_of_course='Python'
+time_for_one=hours_spent/homework_complet
+print ('Курс:',name_of_course,', всего задач:',homework_complet,', затрачено часов:',hours_spent,', среднее время выполнения',time_for_one, 'часа.')
